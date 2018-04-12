16b556d2a8e763cd1213ceda52584148eb56a4b4
Sangwoo Han <realappsdev@gmail.com>
2018-04-12 17:48:21 +0900

    Add api.hycomputing.org to ALLOWED_HOSTS

--- api/api/settings/production.py
+++ api/api/settings/production.py
@@ -10,3 +10,7 @@ DEBUG = False
 with open(os.path.join(BASE_DIR, 'secret.json')) as secret_file:
     secret = json.load(secret_file)
     SECRET_KEY = secret['SECRET_KEY']
+
+ALLOWED_HOSTS = [
+    'api.hycomputing.org',
+]
