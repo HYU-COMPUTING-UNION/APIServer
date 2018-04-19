46e1e0fdca74848182b227e6ca96c8220fb8be64
Sangwoo Han <realappsdev@gmail.com>
2018-04-20 00:40:38 +0900

    Update django admin for accounts

--- api/accounts/admin.py
+++ api/accounts/admin.py
@@ -1,5 +1,31 @@
-# flake8: noqa
-
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+from .models import AffiliationAuth, User, Profile
 
-# Register your models here.
+
+@admin.register(AffiliationAuth)
+class AffiliationAuthAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'student_id']
+    list_display = ['name', 'student_id']
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'student_id']
+    list_display = ['name', 'student_id', 'is_affiliation_authenticated',
+                    'associate_users']
+    list_filter = ['is_affiliation_authenticated']
+
+    def associate_users(self, obj):
+        return obj.users.count()
+
+
+@admin.register(User)
+class UserAdmin(AuthUserAdmin):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        fieldsets = list(self.fieldsets)
+        fieldsets.insert(1,
+            ('Profile', {'fields': ['profile']}),   # noqa
+        )
+        self.fieldsets = fieldsets
