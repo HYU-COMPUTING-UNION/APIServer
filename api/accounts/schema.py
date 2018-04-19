38e39139d014a21807475132439f284cbce54021
Sangwoo Han <realappsdev@gmail.com>
2018-04-20 00:46:11 +0900

    Add ProfileNode; Login, Authenticate mutation

--- api/accounts/schema.py
+++ api/accounts/schema.py
@@ -3,10 +3,12 @@ import graphene
 
 from api.decorators import login_required, method_decorator
 
-from django.contrib.auth import get_user_model, logout
+from django.contrib.auth import get_user_model, logout, login, authenticate
 from django.utils.translation import gettext_lazy as _
 
 from graphene_django import DjangoObjectType
+
+from .models import Profile, AffiliationAuth
 
 
 # ObjectTyes
@@ -16,6 +18,12 @@ class UserNode(DjangoObjectType):
         model = get_user_model()
         interfaces = (graphene.relay.Node, )
         exclude_fields = ['password']
+
+
+class ProfileNode(DjangoObjectType):
+    class Meta:
+        model = Profile
+        interfaces = (graphene.relay.Node, )
 
 
 # Mutation fields
@@ -28,6 +36,26 @@ class SocialAuth(graphql_social_auth.relay.mutations.SocialAuthMutation):
         return cls(viewer=social.user)
 
 
+class Login(graphene.relay.ClientIDMutation):
+    viewer = graphene.Field(UserNode)
+
+    class Input:
+        username = graphene.String(required=True)
+        password = graphene.String(required=True)
+
+    def mutate_and_get_payload(self, info, **input):
+        username = input.get('username')
+        password = input.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(info.context, user)
+            return Login(viewer=user)
+
+        return Login()
+
+
 class Logout(graphene.relay.ClientIDMutation):
     state = graphene.Boolean(required=True)
 
@@ -35,6 +63,37 @@ class Logout(graphene.relay.ClientIDMutation):
     def mutate_and_get_payload(self, info, **input):
         logout(info.context)
         return Logout(state=True)
+
+
+class Authenticate(graphene.relay.ClientIDMutation):
+    state = graphene.Boolean(required=True)
+
+    class Input:
+        name = graphene.String(required=True)
+        student_id = graphene.String(required=True)
+
+    @method_decorator(login_required)
+    def mutate_and_get_payload(self, info, **input):
+        name = input.get('name')
+        student_id = input.get('student_id')
+        state = AffiliationAuth.objects.filter(
+                    name=name,
+                    student_id=student_id,
+                ).exists()
+        user = info.context.user
+
+        if state:
+            try:
+                profile = Profile.objects.get(name=name, student_id=student_id)
+            except Profile.DoesNotExist:
+                profile = Profile.objects.create(
+                    name=name, student_id=student_id,
+                    is_affiliation_authenticated=True,
+                )
+            user.profile = profile
+            user.save()
+
+        return Authenticate(state=state)
 
 
 # Query and Mutation
@@ -52,3 +111,5 @@ class Mutation:
         description=_('Social authentication'),
     )
     logout = Logout.Field()
+    login = Login.Field()
+    authenticate = Authenticate.Field()
